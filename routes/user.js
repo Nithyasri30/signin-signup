48e4947e0bc9daca7bd28d6489c5e1d8bfffeccb
nithya <nithyachivunuri@gmail.com>
2020-07-26 09:37:40 +0530

    First commit Message

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const { check, validationResult} = require("express-validator/check");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const router = express.Router();
+const auth=require("../middleware/auth");
 
 const User = require("../Schemass/Userschema");
 router.post(
@@ -144,4 +145,14 @@ router.post(
       }
     }
   );
+
+  router.get("/me", auth, async (req, res) => {
+    try {
+      // request.user is getting fetched from Middleware after token authentication
+      const user = await User.findById(req.user.id);
+      res.json(user);
+    } catch (e) {
+      res.send({ message: "Error in Fetching user" });
+    }
+  });
 module.exports = router;
